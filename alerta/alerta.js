dfdfa2a01a1aadc6ae9f62d6fa997423079b15e3
Mágila Morgana da Silva Ferreira <magilamorganasf@hotmail.com>
2026-06-04 18:06:21 -0300

    Melhorias de legibilidade e performance no diretório alerta

--- alerta/alerta.js
+++ alerta/alerta.js
@@ -1,18 +1,17 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { exec } from 'child_process';
+import notifier from 'node-notifier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const caminhoBat = path.join(__dirname, 'mostrar_alerta.bat');
+const pastaPDF = path.resolve(__dirname, '..', '..', 'arquivosPgr', 'pdf');
 
-function alertarFimDoProcesso() {
-	exec(`"${caminhoBat}"`, (error) => {
-    if (error) {
-        console.error('Erro ao executar BAT:', error);
-        return;
-    }
-    console.log('Alerta PDF, executada com sucesso');
-});
+const alertarFimDoProcesso = () => {
+    notifier.notify({
+        title: 'Análise concluída',
+        message: 'Os PDFs foram gerados com sucesso!',
+    });
+    exec(`explorer "${pastaPDF}"`);
 }
 export { alertarFimDoProcesso };
--- alerta/mostrar_alerta.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo "Arquivos PDF salvos com sucesso!"
-echo "Abrindo a pasta de destino... "
-
-explorer "%~dp0..\..\arquivosPgr\pdf"
-
-exit /b 0
